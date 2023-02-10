ca69eccfadb77af4abba0044a95d92e29600479a
feng15423 <xuyufeng@sjtu.edu.cn>
2023-02-10 16:57:01 +0800

    rosbagtools

---
+++ rosbag_tools/src/bag_creator/src/bag_creator_node.cpp
@@ -0,0 +1,280 @@
+#include <iostream>
+#include <fstream>
+
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+#include <sensor_msgs/Image.h>
+#include <boost/foreach.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgcodecs.hpp>
+
+#include <yaml-cpp/yaml.h>
+
+using namespace std;
+
+bool getFilesFromFolder(const std::string &folder,
+                        const std::string &suffix,
+                        std::vector<std::string> &files)
+{
+  boost::filesystem::path path(folder);
+ 
+  if (!boost::filesystem::exists(path) || !boost::filesystem::is_directory(path))
+  {
+      std::cout << "the folder is not exists or is not directory. the folder: " << folder << std::endl;
+      return false;
+  }
+ 
+  boost::filesystem::recursive_directory_iterator end_iter;
+ 
+  for(boost::filesystem::recursive_directory_iterator iter(path); iter != end_iter; ++iter)
+  {
+      //check file
+      if(boost::filesystem::is_regular_file(*iter) && iter->path().string().size() > suffix.size())
+      {
+          std::string curSuffix = iter->path().string().substr(iter->path().string().size() - suffix.size());
+ 
+          if (suffix == curSuffix)
+          {
+              files.push_back(iter->path().string());
+          }
+      }
+      else if(boost::filesystem::is_directory(iter->status()))
+      {
+          std::string dirNext(iter->path().string());
+ 
+          getFilesFromFolder(dirNext, suffix, files);
+      }
+    }
+ 
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        std::cerr << std::endl << "Usage: ./bag_creator config_file_path" << std::endl;
+
+        return 1;
+    }
+    std::string config_file_path = std::string(argv[1]);
+    YAML::Node config_node = YAML::LoadFile(config_file_path);
+
+    std::string output_folder_path = config_node["output_folder_path"].as<std::string>();
+    vector<string>bag_names  = config_node["bag_names"].as<vector<string>>();
+
+    for (string bag_name:bag_names){
+
+        cout << "processing" << bag_name << endl;
+        std::string output_bag_name = bag_name;
+        std::string output_bag_path = output_folder_path + output_bag_name + ".bag";
+
+        rosbag::Bag bag;
+        bag.open(output_bag_path, rosbag::bagmode::Write);
+        // bag.open("/home/xuyufeng/my_files/MH_01_easy.bag", rosbag::bagmode::Read);
+
+        std::string l_cam_topic = config_node["left_cam_topic"].as<std::string>();
+        std::string r_cam_topic = config_node["right_cam_topic"].as<std::string>();
+
+        std::string l_cam_topic_rgb = config_node["left_cam_topic_rgb"].as<std::string>();
+        std::string r_cam_topic_rgb = config_node["right_cam_topic_rgb"].as<std::string>();
+
+        std::vector<double> vTimeStamps;
+        std::vector<double> vTimeStamps_rgb;
+        std::ifstream fTimes;
+        std::string strPathTimes_whole = config_node["timestamp_file_path"].as<std::string>();
+        std::string strPathTimes = strPathTimes_whole + bag_name + ".txt";
+        fTimes.open(strPathTimes.c_str());
+        vTimeStamps.reserve(40000);
+        vTimeStamps_rgb.reserve(40000);
+
+        int down_sample_ratio = config_node["down_sample_ratio"].as<int>();
+
+        std::string exposure_time_path_whole =  config_node["exposure_time_path"].as<std::string>();
+        std::string exposure_time_path =  exposure_time_path_whole + bag_name + "_c4a3a4.txt";
+        std::string exposure_time_path_rgb =  exposure_time_path_whole + bag_name + "_c54d7a.txt";
+
+        vector<double> exposure_time;
+        exposure_time.reserve(50000);
+        vector<double> exposure_time_rgb;
+        exposure_time_rgb.reserve(50000);
+
+        ifstream readStream;
+        readStream.open(exposure_time_path, ios::in);
+        int item_count = 0;
+        int exposure_count = 0;
+        if (readStream.is_open())
+        {
+            string readStr;
+            // indexParent = 0;
+            
+            while (readStream >> readStr)
+            {
+                // dalsa_time.push_back(stold(readStr.c_str()));
+                item_count++;
+                if (readStr.size() > 2 && readStr.size() < 6)
+                {
+                    exposure_time.push_back(std::stod(readStr) / 1000000);
+                    // std::cout << readStr << std::endl;
+                    exposure_count++;
+                }
+            }
+            readStream.close();
+            std::cout << "item_count/4= " << item_count / 4 << std::endl;
+            std::cout << "exposure_count=" << exposure_count << std::endl;
+            assert(item_count / 4 == exposure_count);
+        }
+
+        ifstream readStream_rgb;
+        readStream_rgb.open(exposure_time_path_rgb, ios::in); 
+        item_count = 0;
+        exposure_count = 0;
+        if (readStream_rgb.is_open())
+        {
+            string readStr;
+            // indexParent = 0;
+            
+            while (readStream_rgb >> readStr)
+            {
+                // dalsa_time.push_back(stold(readStr.c_str()));
+                item_count++;
+                if (readStr.size() > 2 && readStr.size() < 6)
+                {
+                    exposure_time_rgb.push_back(std::stod(readStr) / 1000000);
+                    // std::cout << readStr << std::endl;
+                    exposure_count++;
+                }
+            }
+            readStream_rgb.close();
+            std::cout << "item_count/4= " << item_count / 4 << std::endl;
+            std::cout << "exposure_count=" << exposure_count << std::endl;
+            assert(item_count / 4 == exposure_count);
+        }
+
+
+        int flag = 0;
+        while(!fTimes.eof())
+        {
+            std::string s;
+            getline(fTimes,s);
+            if(!s.empty())
+            {
+                std::stringstream ss;
+                ss << s;
+                double t;
+                ss >> t;
+                // vTimeStamps.push_back(t);
+                if (flag % down_sample_ratio == 0){
+                    vTimeStamps.push_back(t + exposure_time[flag] / 2 + 8 / 1000000);
+                    vTimeStamps_rgb.push_back(t + exposure_time_rgb[flag] / 2 + 8 / 1000000);
+                }
+                flag++;  
+            }   
+        }
+
+        const int nTimes = vTimeStamps.size();
+        // std::string image_format = config_node["image_format"].as<std::string>();
+        // int cvflag = cv::IMREAD_GRAYSCALE;
+        // if (image_format == "bgr8"){
+        //     cvflag = cv::IMREAD_COLOR;
+        // }
+
+        std::string l_path_whole = config_node["left_folder_path"].as<std::string>();
+        std::string r_path_whole = config_node["right_folder_path"].as<std::string>();
+
+        std::string l_path = l_path_whole + bag_name + "/c4a3a4/";
+        std::string r_path = r_path_whole + bag_name + "/c4a3a8/";
+        std::string l_path_rgb = l_path_whole + bag_name + "/c54d7a/";
+        std::string r_path_rgb = r_path_whole + bag_name + "/c54d79/";
+        // get prefix
+        std::vector<string> file_name_vector_l_gray;
+        getFilesFromFolder(l_path, "tif", file_name_vector_l_gray);
+        
+        std::vector<string> file_name_vector_r_gray;
+        getFilesFromFolder(r_path, "tif", file_name_vector_r_gray);
+       
+        std::vector<string> file_name_vector_l_rgb;
+        getFilesFromFolder(l_path_rgb, "tif", file_name_vector_l_rgb);
+
+        std::vector<string> file_name_vector_r_rgb;
+        getFilesFromFolder(r_path_rgb, "tif", file_name_vector_r_rgb);
+
+        // for (size_t i = 0; i < nTimes; i++)
+        for (size_t i = 0; i < nTimes; i++)
+        {
+            // 1:left  2:right
+            int n = file_name_vector_l_gray[0].size();
+            std::string file_name = file_name_vector_l_gray[0].substr(n - 44, 27);
+            std::string file_name2 = file_name_vector_r_gray[0].substr(n - 44, 27); 
+            std::string file_name3 = file_name_vector_l_rgb[0].substr(n - 44, 27);
+            std::string file_name4 = file_name_vector_r_rgb[0].substr(n - 44, 27); 
+            // std::string file_name = config_node["left_cam_prefix"].as<std::string>();
+            // std::string file_name2 = config_node["right_cam_prefix"].as<std::string>();
+            int batch = i*down_sample_ratio/3000;
+            std::stringstream ss;
+            ss << std::setfill('0') << std::setw(6) << i*down_sample_ratio+1;
+            std::stringstream sss;
+            sss << std::setfill('0') << std::setw(6) << batch;
+            file_name = file_name + sss.str() + "_" + ss.str() + ".tif";
+            file_name2 = file_name2 + sss.str() + "_" + ss.str() + ".tif";
+            file_name3 = file_name3 + sss.str() + "_" + ss.str() + ".tif";
+            file_name4 = file_name4 + sss.str() + "_" + ss.str() + ".tif";
+
+            cv::Mat img = cv::imread(l_path + file_name, cv::IMREAD_GRAYSCALE);
+            if (img.empty()){
+                std::cout << "failed to read: " << l_path << file_name << std::endl;
+                std::cout << "SHUT DOWN!" << std::endl;
+                return 0;
+            }
+            cv::Mat img2 = cv::imread(r_path + file_name2, cv::IMREAD_GRAYSCALE);
+            if (img2.empty()){
+                std::cout << "failed to read: " << r_path <<file_name2 << std::endl;
+                std::cout << "SHUT DOWN!" << std::endl;
+                return 0;
+            }
+            cv::Mat img3 = cv::imread(l_path_rgb + file_name3, cv::IMREAD_COLOR);
+            if (img3.empty()){
+                std::cout << "failed to read: " << l_path_rgb << file_name3 << std::endl;
+                std::cout << "SHUT DOWN!" << std::endl;
+                return 0;
+            }
+            cv::Mat img4 = cv::imread(r_path_rgb + file_name4, cv::IMREAD_COLOR);
+            if (img4.empty()){
+                std::cout << "failed to read: " << r_path_rgb <<file_name4 << std::endl;
+                std::cout << "SHUT DOWN!" << std::endl;
+                return 0;
+            }
+
+            sensor_msgs::ImagePtr ros_image_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", img).toImageMsg();
+            sensor_msgs::ImagePtr ros_image_msg2 = cv_bridge::CvImage(std_msgs::Header(), "mono8", img2).toImageMsg();
+
+            sensor_msgs::ImagePtr ros_image_msg3 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img3).toImageMsg();
+            sensor_msgs::ImagePtr ros_image_msg4 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img4).toImageMsg();
+
+            ros_image_msg->header.stamp = ros::Time().fromSec(vTimeStamps[i]);
+            ros_image_msg->header.frame_id = l_cam_topic;
+            bag.write(l_cam_topic, ros::Time().fromSec(vTimeStamps[i]), ros_image_msg);
+
+            ros_image_msg2->header.stamp = ros::Time().fromSec(vTimeStamps[i]);
+            ros_image_msg2->header.frame_id = r_cam_topic;
+            bag.write(r_cam_topic, ros::Time().fromSec(vTimeStamps[i]), ros_image_msg2);
+
+            ros_image_msg3->header.stamp = ros::Time().fromSec(vTimeStamps_rgb[i]);
+            ros_image_msg3->header.frame_id = l_cam_topic_rgb;
+            bag.write(l_cam_topic_rgb, ros::Time().fromSec(vTimeStamps_rgb[i]), ros_image_msg3);
+
+            ros_image_msg4->header.stamp = ros::Time().fromSec(vTimeStamps_rgb[i]);
+            ros_image_msg4->header.frame_id = r_cam_topic_rgb;
+            bag.write(r_cam_topic_rgb, ros::Time().fromSec(vTimeStamps_rgb[i]), ros_image_msg4);
+
+            std::cout << "wrote frame: " << i+1 << std::endl;
+        }
+
+        std::cout << "---------end---------" << std::endl;
+        
+        bag.close();
+    }
+
+    return 0;
+}
---
+++ rosbag_tools/src/bag_creator/src/bag_extractor_node.cpp
@@ -0,0 +1,67 @@
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+#include <sensor_msgs/Image.h>
+#include <boost/foreach.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgcodecs.hpp>
+
+#include <yaml-cpp/yaml.h>
+
+
+int main(int argc, char **argv)
+{
+    rosbag::Bag bag;
+    std::string config_file_path = std::string(argv[1]);
+    YAML::Node config_node = YAML::LoadFile(config_file_path);
+    std::string input_bag_path = config_node["input_bag_path"].as<std::string>();
+    std::string bag_name = config_node["bag_name"].as<std::string>();
+    std::string bag_path = input_bag_path + bag_name;
+
+    std::string output_folder_path = config_node["output_folder_path"].as<std::string>();
+    std::string output_image_prefix = config_node["output_image_prefix"].as<std::string>();
+    std::string output_image_format = config_node["output_image_format"].as<std::string>();
+
+    bag.open(bag_path, rosbag::bagmode::Read);
+
+    std::string l_cam = config_node["left_cam_topic"].as<std::string>();
+    std::string r_cam = config_node["right_cam_topic"].as<std::string>();
+
+    int down_sample_ratio = config_node["down_sample_ratio"].as<int>();
+
+    // Image topics to load
+    std::vector<std::string> topics;
+    topics.push_back(l_cam);
+    topics.push_back(r_cam);
+
+    rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+    // ros::Time bag_begin_time = view.getBeginTime();
+    // ros::Time bag_end_time = view.getEndTime();
+
+    // std::cout << "ROS bag time: " << (bag_end_time-bag_begin_time).toSec() << "(s)" << std::endl;
+    int value = 0;
+    BOOST_FOREACH(rosbag::MessageInstance const m, view){
+        if (m.getTopic() == l_cam || ("/" + m.getTopic() == l_cam))
+        {
+            value++;
+            if (value % down_sample_ratio != 0){
+                continue;
+            }
+            const ros::Time time = m.getTime();
+            std::cout << time << std::endl;
+            sensor_msgs::Image::ConstPtr l_img = m.instantiate<sensor_msgs::Image>();
+            if (l_img != NULL){
+                cv_bridge::CvImagePtr cv_ptr;
+                cv_ptr = cv_bridge::toCvCopy(l_img, l_img->encoding);
+                std::string s = std::to_string(value);
+                std::string file_name = output_folder_path + output_image_prefix + s + output_image_format;                
+                cv::imwrite(file_name, cv_ptr->image);
+            }
+        }
+    }
+    
+    bag.close();
+
+    return 0;
+}
